40e0b51ec67d6ab283925aef4082c1cad4e36931
Jeskozys <manaadacuy18@gmail.com>
2026-02-02 23:53:25 +0700

    ok

--- run-monitor.bat
+++ run-monitor.bat
@@ -1,15 +1,16 @@
 @echo off
 title KAI Network Monitor
 
-:: 1. Masuk ke folder project Anda
+:: 1. Masuk ke folder project
 cd /d "C:\laragon\www\SistemMonitoringJaringan-KAI-DAOP-3-CIREBON"
 
 :loop
 echo Memulai Monitoring Jaringan...
 
-:: 2. Jalankan perintah monitoring menggunakan PHP Laragon yang spesifik
-"C:\laragon\bin\php\php-8.3.26-Win32-vs16-x64\php.exe" artisan device:check
+:: 2. Jalankan monitoring (Menggunakan versi 8.3.28 sesuai screenshot kamu)
+"C:\laragon\bin\php\php-8.3.28-Win32-vs16-x64\php.exe" artisan device:check
 
-echo Proses berhenti atau crash! Memulai ulang dalam 5 detik...
+echo Proses berhenti atau crash!
+echo Memulai ulang dalam 5 detik...
 timeout /t 5 >nul
 goto loop
